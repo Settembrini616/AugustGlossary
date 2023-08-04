d1556239894e1368706412e29be7a8931b915cc8
Ihor Polovyi <ipolovyi94@gmail.com>
2023-08-04 16:33:45 -0600

    My first commit

---
+++ src/Pages/MainPage/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className='main-main'>
+
+<div className='main-container'>
+   <section className='inner hero-image'>Kailash</section>
+   <aside className='inner login-form'>Login Form</aside>
+   <aside className='inner track-form'>Track your progress</aside>
+   <div className='inner bottom-cards'>Bottom cards</div>
+   
+
+
+
+    </div>
+
+    </div>
+    
+);
+
+    }
+
+
+export default Main;
